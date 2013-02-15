b9931c7d8166a3ae1dabac77d87c9979c2c5016d
Pau Fracés <pfraces@gmail.com>
2013-02-15 22:03:37 +0100

    move({to}) accepts either DOM element or id

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var type = require('type');
+
 var arrange = module.exports = {
-  moveTo: function (args) {
-    args.sel(args.id).appendChild(args.node);
+  move: function (args) {
+    var to = args.to;
+    (type(to).isString() ? args.sel(to) : to).appendChild(args.node);
   },
   del: function (args) {
     var node = args.node;
