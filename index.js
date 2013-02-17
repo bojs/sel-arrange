deb149603f9d34c880fdbb488a4c6eca482eebc2
Pau Fracés <pfraces@gmail.com>
2013-02-18 00:18:46 +0100

    simplifying: move only accepts a sel instance

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ var type = require('type');
 
 var arrange = module.exports = {
   move: function (args) {
-    (function (target) {
-      if (type(target).isString()) {
-        return args.sel(target).first();
-      } else if (target instanceof Sel) {
-        return target.first();
-      } else return target;
-    })(args.to).appendChild(args.node);
+    args.to.first().appendChild(args.node);
   },
   del: function (args) {
     var node = args.node;
